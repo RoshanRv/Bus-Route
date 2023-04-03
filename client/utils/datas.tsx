a35afb4282ba1502691ee580325b1f26f9272406
RoshanRv <nkroshankumar@gmail.com>
2023-04-03 11:24:39 +0530

    busNo datas...

---
+++ client/utils/datas.tsx
@@ -0,0 +1,34 @@
+export const buses = [
+    {
+        value: "36A",
+        label: "36A",
+    },
+    {
+        value: "30A",
+        label: "30A",
+    },
+    {
+        value: "24A",
+        label: "24A",
+    },
+    {
+        value: "52A",
+        label: "52A",
+    },
+    {
+        value: "43B",
+        label: "43B",
+    },
+    {
+        value: "77A",
+        label: "77A",
+    },
+    {
+        value: "88C",
+        label: "88C",
+    },
+    {
+        value: "40C",
+        label: "40C",
+    },
+]
